64c54d465c30631f784df4fa215a0295ce203628
caihong <caihong@163.com>
2024-04-19 22:33:08 +0800

    模仿其他组件实现

--- src/main/java/com/learn/more/Connection.java
+++ src/main/java/com/learn/more/Connection.java
@@ -1,4 +1,4 @@
 package com.learn.more;
 
-public class Connection {
+public interface Connection {
 }
--- src/main/java/com/learn/more/Dispatcher.java
+++ src/main/java/com/learn/more/Dispatcher.java
@@ -3,12 +3,17 @@ package com.learn.more;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+// todo 支持异步任务
 public class Dispatcher {
   private Runnable idleCallback;
 
   private final Deque<RealCall> runningSyncCalls = new ArrayDeque<>();
 
   public Dispatcher() {
+  }
+
+  public void setIdleCallback(Runnable idleCallback) {
+    this.idleCallback = idleCallback;
   }
 
   synchronized void executed(RealCall call) {
